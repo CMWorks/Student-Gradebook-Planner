c88f5911a6e501167a6d0a6a8ea52d6092845408
Eric Deck <deck.eric@ymail.com>
2021-11-11 16:04:58 -0600

    Added popup. Added more to the course page.

---
+++ src/components/Popup.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Popup.css'
+
+function Popup(props) {
+    return (props.trigger) ? (
+        <div className="popup">
+            <div className="popup-inner">
+                {
+                    props.children
+                }
+            </div>
+            
+        </div>
+    ) : "";
+}
+
+export default Popup
