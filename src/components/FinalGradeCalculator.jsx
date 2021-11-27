59f617a91a82bda78517c88ee6f6832ab1cb455d
levi-schaut <schaut.levi@gmail.com>
2021-11-26 19:49:43 -0600

    Cleaned up the Final Grade Calculator page and tried installing the server on my local branch.

--- src/components/FinalGradeCalculator.jsx
+++ src/components/FinalGradeCalculator.jsx
@@ -1,13 +1,16 @@
 import React from "react";
+import Popup from "./Popup";
 
 class FinalGradeCalculator extends React.Component
 {
     constructor(props) {
         super(props);
         this.state = {
-        curGrade: 0,
-        desiredGrade: 0,
-        finalWeight: 0,
+            curGrade: 0,
+            desiredGrade: 0,
+            finalWeight: 0,
+            gradeNeeded: 0,
+            popUpFinalGrade: false,
         };
 
         this.handleChange = this.handleChange.bind(this);
@@ -15,9 +18,8 @@ class FinalGradeCalculator extends React.Component
     }
 
     handleChange(event) {
-        const target = event.target;
-        const value = +target.value;
-        const name = target.name;
+        const value = +event.target.value;
+        const name = event.target.name;
 
         this.setState({[name]: value});
     }
@@ -26,57 +28,57 @@ class FinalGradeCalculator extends React.Component
         const curGrade = this.state.curGrade;
         const desiredGrade = this.state.desiredGrade;
         const finalWeight = this.state.finalWeight / 100;
-        let gradeNeeded = ( desiredGrade  -  curGrade * (1-finalWeight) )  /  finalWeight;
-        alert('curGrade = ' + curGrade + '\ndesiredGrade = ' + desiredGrade + '\nfinalWeight = ' + finalWeight + "\ngradeNeeded = " + gradeNeeded);
+        let localGradeNeeded = ( desiredGrade  -  curGrade * (1-finalWeight) )  /  finalWeight;
+        this.setState({gradeNeeded: localGradeNeeded});
+        // alert('curGrade = ' + curGrade + '\ndesiredGrade = ' + desiredGrade + '\nfinalWeight = ' + finalWeight + "\ngradeNeeded = " + gradeNeeded);
+        this.setState({popUpFinalGrade: true});
         event.preventDefault();
     }
 
     render() {
         return (
         <div>
-            {/* Input for current grade. */}
-            <form>
-            <label>
-                Current Grade:
-                <input 
-                name="curGrade"
-                type="number"
-                value={this.state.curGrade}
-                onChange={this.handleChange}
-                />
-            </label>
-            </form>
+            <div className="container">
+                <Popup trigger={this.state.popUpFinalGrade}>
+                    <h3>Result:</h3>
+                    <div>
+                        <label>You need a {this.state.gradeNeeded}% on your final in order to get a {this.state.desiredGrade}% overall.</label>
+                    </div>
+                    <button className="btn btn-primary" onClick={() => this.setState({ popUpFinalGrade: false })}> Okay </button>
+                </Popup>
 
-            {/* Input for desired grade. */}
-            <form>
-            <label>
-                Desired Grade:
-                <input 
-                name="desiredGrade"
-                type="number"
-                value={this.state.desiredGrade}
-                onChange={this.handleChange}
-                />
-            </label>
-            </form>
+                {/* Input for current grade. */}
+                <form>
+                    <label>
+                        Current grade is
+                        <input name="curGrade" type="number" value={this.state.curGrade} onChange={this.handleChange} />
+                        %.
+                    </label>
+                </form>
 
-            {/* Input for exam weight. */}
-            <form>
-            <label>
-                Final Exam Weight (%):
-                <input 
-                name="finalWeight"
-                type="number"
-                value={this.state.finalWeight}
-                onChange={this.handleChange}
-                />
-            </label>
-            </form>
+                {/* Input for desired grade. */}
+                <form>
+                    <label>
+                        I want a 
+                        <input name="desiredGrade" type="number" value={this.state.desiredGrade} onChange={this.handleChange} />
+                        % in the class.
+                    </label>
+                </form>
 
-            {/* Submit */}
-            <form name="submit" onSubmit={this.handleSubmit}>
-            <input type="submit" value="Calculate Answer" />
-            </form>
+                {/* Input for exam weight. */}
+                <form>
+                    <label>
+                        Final is worth 
+                        <input name="finalWeight" type="number" value={this.state.finalWeight} onChange={this.handleChange} />
+                        % of total grade.
+                    </label>
+                </form>
+
+                {/* Submit */}
+                <form name="submit" onSubmit={this.handleSubmit}>
+                    <input type="submit" value="Calculate Answer" />
+                </form>
+            </div>
         </div>
         )
     }
